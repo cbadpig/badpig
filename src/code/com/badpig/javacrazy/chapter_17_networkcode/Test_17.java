58ab8be5ef83b883bbe8ee682ef418e8ee66fed1
cbadpig <891511545@qq.com>
2018-12-17 10:25:22 +0800

    cbadpig 的知识库

---
+++ src/code/com/badpig/javacrazy/chapter_17_networkcode/Test_17.java
@@ -0,0 +1,17 @@
+package code.com.badpig.javacrazy.chapter_17_networkcode;
+
+import code.com.badpig.javacrazy.chapter_17_networkcode.demo_1_InetAddress.InetAddress_A;
+import org.junit.jupiter.api.Test;
+
+public class Test_17 {
+
+    @Test
+    public void inetAddress_A_demo_a () {
+        InetAddress_A inetAddress_A = new InetAddress_A();
+        inetAddress_A.InetAddressDemo_1();
+
+
+
+    }
+
+}
